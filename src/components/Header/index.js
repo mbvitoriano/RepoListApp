907ac69efd76f42d63b17fb2b57804eebc2c7328
Matheus Vitoriano <matheusbzv@gmail.com>
2020-02-09 17:12:46 -0300

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './styles.css';
+
+
+const Header = () => <header id="main-header">Repositories List</header>
+
+export default Header;
---
+++ src/pages/main/index.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import api from '../../services/api';
+import './styles.css';
+import { Link } from 'react-router-dom';
+
+export default class main extends Component {
+  state = {
+    products: [],
+    productInfo: [],
+    page: 1,
+  }
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
+
+    const {docs, ...productInfo } = response.data;
+
+    this.setState({products: docs, productInfo, page})
+  }
+
+  prevPage = () => {
+    const { page, productInfo}  = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  }
+
+  nextPage = () => {
+    const { page, productInfo}  = this.state;
+    if (page === productInfo.pages) return;
+    
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
+  }
+
+  render() {
+
+    const {products, page, productInfo} = this.state;
+  return (
+    <div className='product-list'>
+      {products.map(product => (
+        <article key={product._id}>
+          <strong>{product.title}</strong>
+          <p>{product.description}</p>
+          <Link to={`/Products/${product._id}`}>Access</Link>
+        </article>
+      ))}
+      <div className='actions'>
+        <button disabled={page === 1} onClick={this.prevPage}>Previous</button>
+        <button disabled={page === productInfo.pages} onClick={this.nextPage}>Next</button>
+      </div>
+    </div>
+  );
+  }
+}
